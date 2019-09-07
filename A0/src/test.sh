7705368fdb1c900d0d5f9306cf54395ebb809305
JSonWulff <julianwulff@gmail.com>
2019-09-07 15:50:14 +0200

    Created test plan

--- A0/src/test.sh
+++ A0/src/test.sh
@@ -17,9 +17,26 @@ printf "Hello, World!" > test_files/ascii2.input
 printf "Hello,\x00World!\n" > test_files/data.input
 printf "" > test_files/empty.input
 ### TODO: Generate more test files ###
+printf "hemmelighed" > secret.input
+chmod -r secret.input
 # Create tests for number of arguments on program
 # Create tests files that dosen't exist
 # Create tests for "hemmelig_fil" / Permission denied
+
+### ASCII
+# Alle tegn
+# Stor fil
+# Lille fil
+# Fil med masse tal / hex
+# Et enkelt char
+
+
+### DATA
+# Mange tegn
+# Stor fil
+# Lille fil
+# NULL Bytes
+# Et enkelt data tegn
 
 
 echo "Running the tests.."
@@ -38,4 +55,6 @@ do
     echo ">>> Success :-)"
   fi
 done
+
+chmod +r secret.input
 exit $exitcode
