0ddb39dad49196f2e08ee3add244d5d0dfe3af71
Mads Christensen <mads.bchrist@gmail.com>
2019-09-07 15:31:07 +0200

    Comparison + gitignore

--- A0/src/file.c
+++ A0/src/file.c
@@ -3,6 +3,24 @@
 #include <errno.h>  // errno
 #include <string.h> // strerror
 
+enum file_type {
+  DATA = 0,
+  EMPTY = 1,
+  ASCII = 2,
+};
+
+enum file_type cur_type = 1;
+
+enum file_type FindType(){
+  return cur_type;
+}
+
+const char * const FILE_TYPE_STRINGS[] = {
+  "data",
+  "empty",
+  "ASCII text",
+};
+
 int print_hello_world() {
   return fprintf(stdout, "Hello, world!\n");
 }
@@ -13,7 +31,6 @@ int print_error(char *path, int errnum) {
 
 int main(int argc, char* argv[]) {
   int retval = EXIT_SUCCESS;
-
   // Check correct number of arguments
   if (argc == 1){
     printf("Usage: file path\n");
@@ -29,8 +46,6 @@ int main(int argc, char* argv[]) {
 
     // Check that file exist
     if (f != NULL) {
-      printf("Opening:%s\n", argv[1]);
-      
       // Read file from start to end 
       while (1) {
         char b;
@@ -41,6 +56,14 @@ int main(int argc, char* argv[]) {
           break;
         }
 
+        if (b == 0 && FindType() != DATA && FindType() != ASCII){
+          cur_type = EMPTY;
+        } else if (((b >= 0x07 && b <= 0x0D) || b == 0x1B || (b >= 0x20 && b <= 0x7E)) && FindType() != DATA) {
+          cur_type = ASCII;
+        } else {
+          cur_type = DATA;
+          break;
+        }
         // Check chars
       }
       
@@ -48,6 +71,16 @@ int main(int argc, char* argv[]) {
       print_error(argv[1],errno);
       retval = EXIT_FAILURE;
     }
+    
+    if(FindType() == DATA) {
+      fprintf(stdout,"%s: data", filename);
+    }else if (FindType() == EMPTY)
+    {
+      fprintf (stdout,"%s: empty", filename);
+    } else {
+      fprintf (stdout,"%s: ASCII text", filename);
+    }
+    fprintf(stdout,"\n");
 
     fclose(f);
 
