a82bf8ce060b1ef2ecd93e9200f2b4a343f10514
JSonWulff <julianwulff@gmail.com>
2019-09-17 14:23:22 +0200

    Split the main function into smaller functions

--- A1/src/file.c
+++ A1/src/file.c
@@ -1,81 +1,75 @@
-#include <stdio.h>  // fprintf, stdout
-#include <stdlib.h> // EXIT_FAILURE, EXIT_SUCCESS.
-#include <errno.h>  // errno
-#include <string.h> //  strerror
+#include <errno.h>   // errno
+#include <stdio.h>   // fprintf, stdout
+#include <stdlib.h>  // EXIT_FAILURE, EXIT_SUCCESS.
+#include <string.h>  //  strerror
 
 enum file_type {
-  DATA ,
+  DATA,
   EMPTY,
   ASCII,
 };
 
-const char * const FILE_TYPE_STRINGS[] = {
-  "data",
-  "empty",
-  "ASCII text",
+const char* const FILE_TYPE_STRINGS[] = {
+    "data",
+    "empty",
+    "ASCII text",
 };
 
 enum file_type cur_type;
 
-int print_error(char *path, int errnum) {
+int print_error(char* path, int errnum) {
   return fprintf(stdout, "%s: Cannot determine (%s)\n", path, strerror(errnum));
 }
 
+int print_hello_world() { return fprintf(stdout, "Hello, world!\n"); }
 
+void getFileType(FILE* file) {
+  int i = 0;
+  while (1) {
+    char b;
+    int read = fread(&b, 1, 1, file);
 
-int print_hello_world() {
-  return fprintf(stdout, "Hello, world!\n");
+    if (read == 0 && i == 0) {
+      cur_type = EMPTY;
+      break;
+    } else if (read == 0) {
+      break;
+    } else if ((b >= 0x07 && b <= 0x0D) || b == 0x1B ||
+               (b >= 0x20 && b <= 0x7E)) {
+      cur_type = ASCII;
+    } else {
+      cur_type = DATA;
+      break;
+    }
+
+    i++;
+  }
 }
 
 int main(int argc, char* argv[]) {
   int retval = EXIT_SUCCESS;
   // Check correct number of arguments
-  if (argc == 1){
+  if (argc == 1) {
     fprintf(stderr, "Usage: file path\n");
     retval = EXIT_FAILURE;
   } else if (argc > 2) {
     fprintf(stderr, "To many arguments given\n");
     retval = EXIT_FAILURE;
   } else {
-
     char* filename = argv[1];
 
     FILE* f = fopen(filename, "r");
 
-    // Check that file exist
     if (f != NULL) {
-      // Read file from start to end 
-      int i = 0;
-      while (1) {
-        char b;
-        int read = fread(&b, 1, 1, f);
-
-        // Stop loop when file is read to the end or empty
-        if (read == 0 && i == 0) {
-          cur_type = EMPTY;
-          break;
-        } else if (read == 0) {
-          break;
-        } else if ((b >= 0x07 && b <= 0x0D) || b == 0x1B || (b >= 0x20 && b <= 0x7E)) {
-          cur_type = ASCII;
-        } else {
-          cur_type = DATA;
-          break;
-        }
-
-        i++;
-      }
-      
+      getFileType(f);
     } else {
-      print_error(argv[1],errno);
+      fclose(f);
+      print_error(argv[1], errno);
       retval = EXIT_FAILURE;
       return retval;
     }
-    
-    fprintf(stdout, "%s: %s\n", filename, FILE_TYPE_STRINGS[cur_type] );
 
-    fclose(f);
-
+    fprintf(stdout, "%s: %s\n", filename, FILE_TYPE_STRINGS[cur_type]);
   }
   return retval;
 }
