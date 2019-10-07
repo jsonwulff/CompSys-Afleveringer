bfdde6801185947de664318756ac1b7ba202e549
JSonWulff <julianwulff@gmail.com>
2019-10-07 10:36:50 +0200

    Fixed .gitignore

--- A1/src/program1.c
+++
@@ -1,5 +0,0 @@
-long p(long a1) {
-  temp 1 = a1 >> 63;
-  temp 2 = temp1 ^ a1;
-  return temp 2 - temp1;
-}
--- A2/src/support.h
+++
@@ -1,7 +0,0 @@
-/*
-  Support functions. Just one for now.
-*/
-
-// print an error and exit
-void error(const char* message);
-
